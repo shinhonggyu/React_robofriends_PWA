918b043648482f294bb39a902c8924c076b57ce2
shinhonggyu <zowoz0@naver.com>
2021-05-20 00:43:38 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import reportWebVitals from './reportWebVitals';
 import App from './containers/App';
 import 'tachyons';
 import { searchRobots, requestRobots } from './reducers';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 const logger = createLogger();
 
@@ -29,3 +30,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
